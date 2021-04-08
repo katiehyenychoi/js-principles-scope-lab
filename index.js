fecea03b35a4a675edc2cf256f129c66d4ddf425
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-08 18:24:31 -0400

    commit tree 3d58366fecf4731e592c73a92a4e646f0edd903d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// function setBestCustomer() {
+//      bestCustomer = "not bob";
+// }
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+
+function overwriteBestCustomer(revision) {
+    bestCustomer = revision;
+}
+
+
+const leastFavoriteCustomer = "initial";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "later";
+}
